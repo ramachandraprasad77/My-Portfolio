20517d8ab5ba36d2f81a65e2cce7a11a29c6e2fb
Ramachandraprasad Sharma M <ramachandraprasad77@gmail.com>
2025-05-22 20:07:15 +0530

    final

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: "postcss.config.js",
-  },
+  server: {
+    mimeTypes: {
+      'application/javascript': ['js', 'jsx'],
+      'text/javascript': ['js', 'jsx']
+    }
+  }
 });
