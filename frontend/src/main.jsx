03874ed3a817777d8af22461290d2c61434c3ec4
Ramachandraprasad Sharma M <ramachandraprasad77@gmail.com>
2025-05-22 20:04:56 +0530

    final

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,10 @@
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    mimeTypes: {
-      'application/javascript': ['js', 'jsx'],
-      'text/javascript': ['js', 'jsx']
-    }
-  }
-});
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
